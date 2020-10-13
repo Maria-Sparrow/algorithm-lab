0cdc551616862d3b9f67aa457ebcfc713a17fdd4
Maria-Sparrow <53764665+Maria-Sparrow@users.noreply.github.com>
2020-10-13 11:57:12 +0300

    added main file with smth change

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ file = "basin.csv"
 if len(sys.argv) != 0:
     file = sys.argv[0]
 
-if __name__ == '__main__':
     list_of_basins = get_objects_from_csv(file)
     list_for_selection = copy.deepcopy(list_of_basins)
     print('-------------------------------------------')
     start = timer()
-    elapsed_time = timedelta(((timer() - start) * 1000))
-    counter_meth.print_counter_result(elapsed_time, 'SELECTION')
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     selection_sort_by_volume(list_for_selection)
+    elapsed_time = timedelta(((timer() - start) * 1000))
+    counter_meth.print_counter_result(elapsed_time, 'SELECTION')
 
 
     for basin in list_for_selection:
@@ -25,9 +24,9 @@ if __name__ == '__main__':
     print('--------------------------------------------')
 
     start = timer()
+    list_of_basins = merge_sort_by_visitors(list_of_basins)
     elapsed_time = timedelta(((timer() - start) * 1000))
     counter_meth.print_counter_result(elapsed_time, 'MERGE')
-    elapsed_time = timedelta(((timer() - start) * 1000))
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     for basin in list_of_basins:
         print(str(basin))
